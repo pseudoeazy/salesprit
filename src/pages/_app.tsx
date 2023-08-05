f9787eb206150e3a27c443931c1602806bdd9f14
israel <israel4sng@yahoo.com>
2023-08-05 17:38:24 +0800

    clean _app.tx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,16 @@
-import "assets/styles/globals.css";
-import "assets/styles/nprogress.css"; // Import NProgress stylesheet here
 import { useEffect } from "react";
+import type { AppProps } from "next/app";
 import Router from "next/router";
 import NProgress from "nprogress";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
 import { Outfit } from "next/font/google";
+import { ChakraProvider ,extendTheme} from "@chakra-ui/react";
 import { SearchProvider } from "contexts/search/use-search";
 import { CartProvider } from "contexts/cart/cart-provider";
-// import "react-toastify/dist/ReactToastify.css";
-// import "overlayscrollbars/overlayscrollbars.css";
-// 1. Import the extendTheme function
-import { extendTheme } from "@chakra-ui/react";
+import "assets/styles/globals.css";
+import "assets/styles/nprogress.css"; // Import NProgress stylesheet here
 
-// 2. Extend the theme to include custom colors, fonts, etc
+
+// Extend the theme to include custom colors, fonts, etc
 const colors = {
   primary: "#003048",
   secondary: "#F67F20",
